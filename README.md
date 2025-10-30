8f3ea3b431bbd784d04d0eff976ef0b8aedea886
Tasal <tasalislam@gmail.com>
2025-10-30 21:41:41 +0000

    Created the main buttons for the calculator
    The buttons are the numbers and the operators which are implemnetd -
    in the index.html file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Calculator
 Calculator project
+
+This project is about making a calculator. It will test everything i have learned so far about Html, CSS and javascript.
---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function Multiply(a, b) {
+    return a * b;
+}
+
+function Divide(a, b) {
+    if (b == 0) {
+        return "error, you cant divide by 0";
+    }
+    
+    return a / b
+}
